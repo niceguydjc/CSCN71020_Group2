07e42306f950438ad4ef7e07c278adaa8c5c2730
Denim MacDougall <dmacdougall7987@conestogac.on.ca>
2024-03-04 10:08:26 -0500

    created unit test outline

---
+++ Unit Test/Unit Test.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace input
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
+namespace triangle
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
+namespace rectangle
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}

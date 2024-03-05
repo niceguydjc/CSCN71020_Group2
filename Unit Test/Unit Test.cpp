c4903cc123203fea3a9ed670818b3e4ef5064560
Denim MacDougall <dmacdougall7987@conestogac.on.ca>
2024-03-05 00:56:45 -0500

    reworked the test for createSquare()

--- Unit Test/Unit Test.cpp
+++ Unit Test/Unit Test.cpp
@@ -20,11 +20,9 @@ typedef struct rectangle {
 	int perimeter;
 } RECTANGLE;
 
-extern "C" struct rectangle createRectangle(int ArrayOfPoints[4][2]);
+extern "C" RECTANGLE createRectangle(int ArrayOfPoints[4][2]);
+extern "C" char* analyzeTriangle(int, int, int);
 
-
-
-int equalRectangles(RECTANGLE rectangle1, RECTANGLE rectangle2);
 
 namespace input
 {
@@ -75,64 +73,56 @@ namespace rectangles
 
 		}
 	};
-	TEST_CLASS(exceptions)
+	TEST_CLASS(createSquare)
 	{
 	public:
+		int InputArray[4][2] = {
+			{8,2}, {2, 2}, {8, 6}, {2, 6}
+		};
+		RECTANGLE actual = createRectangle(InputArray);
 
-		TEST_METHOD(Test001)
-		{
-			RECTANGLE expected;
-			expected.bottomRightCorner.x = 8;
-			expected.bottomRightCorner.y = 2;
-			expected.bottomLeftCorner.x = 2;
-			expected.bottomLeftCorner.y = 2;
-			expected.topRightCorner.x = 8;
-			expected.topRightCorner.y = 6;
-			expected.topLeftCorner.x = 2;
-			expected.topLeftCorner.y = 6;
+		TEST_METHOD(Test001){
 
-			int InputArray[4][2] = {
-				{8,2}, {2, 2}, {8, 6}, {2, 6}
-			};
+			Assert::AreEqual(actual.topLeftCorner.x, 2);
 
-			RECTANGLE actual = createRectangle(InputArray);
-			int result = equalRectangles(expected, actual);
-			Assert::AreEqual(result, 1);
+		}
+		TEST_METHOD(Test002) {
+
+			Assert::AreEqual(actual.topLeftCorner.y, 6);
+
+		}
+
+		TEST_METHOD(Test003) {
+
+			Assert::AreEqual(actual.topRightCorner.x, 8);
+
+		}
+		TEST_METHOD(Test004) {
+
+			Assert::AreEqual(actual.topRightCorner.y, 6);
+
+		}
+
+		TEST_METHOD(Test005) {
+
+			Assert::AreEqual(actual.bottomLeftCorner.x, 2);
+
+		}
+		TEST_METHOD(Test006) {
+
+			Assert::AreEqual(actual.bottomLeftCorner.y, 2);
+
+		}
+
+		TEST_METHOD(Test007) {
+
+			Assert::AreEqual(actual.topRightCorner.x, 8);
+
+		}
+		TEST_METHOD(Test008) {
+
+			Assert::AreEqual(actual.topRightCorner.y, 2);
 
 		}
 	};
-}
-
-//compares two rectangle structures, returns 1 if they are equal
-int equalRectangles(RECTANGLE rectangle1, RECTANGLE rectangle2) {
-	//compare top left
-	if (rectangle1.topLeftCorner.x != rectangle2.topLeftCorner.x) {
-		return 0;
-	}
-	if (rectangle1.topLeftCorner.y != rectangle2.topLeftCorner.y) {
-		return 0;
-	}
-	//compare top right
-	if (rectangle1.topRightCorner.x != rectangle2.topRightCorner.x) {
-		return 0;
-	}
-	if (rectangle1.topRightCorner.y != rectangle2.topRightCorner.y) {
-		return 0;
-	}
-
-	if (rectangle1.bottomRightCorner.x != rectangle2.bottomRightCorner.x) {
-		return 0;
-	}
-	if (rectangle1.bottomRightCorner.y != rectangle2.bottomRightCorner.y) {
-		return 0;
-	}
-
-	if (rectangle1.bottomLeftCorner.x != rectangle2.bottomLeftCorner.x) {
-		return 0;
-	}
-	if (rectangle1.bottomLeftCorner.y != rectangle2.bottomLeftCorner.y) {
-		return 0;
-	}
-
-	return 1;
 }
